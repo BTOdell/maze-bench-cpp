5ca7cd97d897a5a693663f63ca64e7d75d238c9b
Bradley Odell <btodell@hotmail.com>
2021-12-10 00:35:32 -0800

    Removed files for now. Trying to include lodepng library.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
 
-#include "png++/png.hpp"
-
-#include "solver.h"
+#include "lodepng.h"
 
 int main() {
-    png::image<png::rgba_pixel> img("maze1000.png");
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
